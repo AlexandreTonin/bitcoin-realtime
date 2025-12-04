3f1ecf2e630048f54bc20b8bcbc9a13d6d1d4f91
Alexandre Tonin <xandetonin@gmail.com>
2025-12-04 20:07:32 -0300

    chore: start java project

---
+++ src/bitcoin_realtime/Bitcoin_realtime.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bitcoin_realtime;
+
+/**
+ *
+ * @author alexandre
+ */
+public class Bitcoin_realtime {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
